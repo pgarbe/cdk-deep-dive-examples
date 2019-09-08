c87e33b265e272131bf4bee471c0f6c0d1f90531
Philipp Garbe <philipp.garbe@rio.cloud>
2019-09-08 12:50:38 +0200

    More examples

--- bin/aws-community-day.ts
+++ bin/aws-community-day.ts
@@ -4,6 +4,8 @@ import cdk = require('@aws-cdk/core');
 import { AwsCommunityDayStack } from '../lib/aws-community-day-stack';
 import { MigrationStack } from '../lib/migration-stack';
 import { ExistingStack } from '../lib/existing-stack';
+import { AspectsStack } from '../lib/aspects-stack';
+import { CustomResourceStack } from '../lib/custom-resource-stack';
 
 const pgarbe  = { account: '424144556073', region: 'eu-west-1' };
 
@@ -12,3 +14,5 @@ const app = new cdk.App();
 new AwsCommunityDayStack(app, 'AwsCommunityDayStack');
 new MigrationStack(app, 'MigrationStack');
 new ExistingStack(app, 'ExistingStack', { env: pgarbe });
+new AspectsStack(app, 'AspectsStack', { env: pgarbe });
+new CustomResourceStack(app, 'CustomResourceStack');
---
+++ lib/custom-resource-stack.ts
@@ -0,0 +1,32 @@
+import cdk = require('@aws-cdk/core');
+import acr = require('@aws-cdk/custom-resources');
+import route53 = require('@aws-cdk/aws-route53');
+
+export class CustomResourceStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const zone = new route53.PublicHostedZone(this, 'HostedZone', {
+      zoneName: 'fully.qualified.domain.com'
+    });
+
+    // https://docs.aws.amazon.com/ses/latest/APIReference/API_VerifyDomainIdentity.html
+    const verifyDomainIdentity = new acr.AwsCustomResource(this, 'VerifyDomainIdentity', {
+      onCreate: {
+        service: 'SES',
+        action: 'verifyDomainIdentity',
+        parameters: {
+          Domain: 'example.com'
+        },
+        physicalResourceIdPath: 'VerificationToken' // Use the token returned by the call as physical id
+      }
+    });
+
+    new route53.TxtRecord(this, 'SESVerificationRecord', {
+      recordName: `_amazonses.example.com`,
+      zone: zone,
+      values: [ verifyDomainIdentity.getData('VerificationToken').toString() ]
+    });    
+  }
+}
+
